d3cd9c1fb93045c513326679e6b9f37bfdfdcfa9
Mohansai Mallineni <mohansaim20@gmail.com>
2025-11-09 09:32:56 +0530

    git

--- app/page.tsx
+++ app/page.tsx
@@ -5,8 +5,7 @@ import "./globals.css";
 export default function Home() {
   return (
     <div className="home-container">
-      <h1 className="home-title">Search and Scrap</h1>
-
+      <h1 className="home-title">Mini Full-Stack Search</h1>
       <div className="link-group">
         <Link href="/Search" className="nav-link search-link">
           Go to Search
